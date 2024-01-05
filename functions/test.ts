a83793caaffb5e122ac8435fe9eaa21d1020e490
Rienz Ivan Otiong <31911993+Riyenz@users.noreply.github.com>
2024-01-05 15:43:22 +0800

    Initial commit

---
+++ functions/test.ts
@@ -0,0 +1,12 @@
+export default async function () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+      },
+      null,
+      2
+    ),
+  };
+}
